f1bbf4e63a09c5ac15fd940e3dd1d3be974dfeb0
John Keaney <trinity.keaney@gmail.com>
2020-12-14 23:39:44 +0000

    Http request working

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 function buttonPress() {
     console.log("Hey there")
     window.alert("What's up")
+
+    main()
 }
+
+async function main() {
+    const url = 'https://api.github.com/users/Keaneyjo/repos'
+
+    const response = await fetch(url)
+//6
+    const result = await response.json()
+    console.log(result)
+}
+
+
+
