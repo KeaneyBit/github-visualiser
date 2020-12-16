b2ee7fa03091dcf4b3ea4b83be74ba6221655e64
John Keaney <trinity.keaney@gmail.com>
2020-12-16 00:32:27 +0000

    Authorization + private repos added

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 
 Useful doc:https://docs.github.com/en/free-pro-team@latest/rest/guides/getting-started-with-the-rest-api
+
+UI - basic : https://colorlib.com/wp/html5-and-css3-login-forms/
--- app.js
+++ app.js
@@ -8,21 +8,30 @@ async function searchUser() {
     const user = loginForm.username.value;
     const authorisation = loginForm.password.value !== "" ? loginForm.password.value : undefined;
 
-    loadData(user, authorisation)
+    let info = await loadData(user, authorisation)
+
+    let img = document.getElementById('img');
+    img.src = info.avatar_url;
+
+    // let repoInfo = await loadRepos(user, authorisation)
+    // console.log(repoInfo);
+
+    let newInfo = await getRequest(user, authorisation)
+    console.log(newInfo.authorisation)
 }
 
-async function httpRequest(url, authorisation) {
-    const headers = {'Authorization': `Token ${authorisation}`}
-    const response = (authorisation == undefined) ? await fetch(url) : await fetch(url, {"method": "GET", "headers": headers});
+async function httpRequest(url, token) {
+    const headers = {'Authorization': `token ${token}`}
+    const response = (token == undefined) ? await fetch(url) : await fetch(url, {"method": "GET", "headers": headers});
     return await response.json();
 }
 
 async function loadData(user, token) {
-    let url = `https://api.github.com/users/${user}/repos`;
+    //let url = `https://api.github.com/users/${user}/repos`;
     
-    let repo = await httpRequest(url, token).catch(error => console.error(error));
+    //let repo = await httpRequest(url, token).catch(error => console.error(error));
 
-    url = `https://api.github.com/users/${user}`;
+    let url = `https://api.github.com/users/${user}`;
     let user_info = await httpRequest(url, token).catch(error => console.error(error));
     
     if (user_info.name != null) {
@@ -31,8 +40,32 @@ async function loadData(user, token) {
     } else {
         loginErrorMsg.style.opacity = 1;
     }
+
+    return user_info;
 }
 
+async function loadRepos(user, token) {
+    let url = `https://api.github.com/users/${user}/repos`;
+    
+    let repo = await httpRequest(url, token).catch(error => console.error(error));
+    return repo;
+}
+
+
+async function getRequest(user, token) {
+    let url = `https://api.github.com/search/repositories?q=user:${user}`;
+    const headers = {
+        "Authorization" : `token 1a7fb31adeb0fe519bbf9ac7e14119d19759e9a4`
+    }
+
+    const response = (token == undefined) ? await fetch(url) : await fetch(url, {
+        "method": "GET",
+        "headers": headers
+    });
+
+    let data = await response.json();
+    return data;
+}
     
 
 
